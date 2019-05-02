9502a562d74f8dfddb8ca9eb01d3c0dfb06b9c76
esofar <973095739@qq.com>
2019-05-02 21:32:43 +0800

    Initial commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        private readonly IDistributedCache _distributedCache;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HelloController(
+            IDistributedCache distributedCache,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _distributedCache = distributedCache;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var connection = _httpContextAccessor.HttpContext.Connection;
+            var ipv4 = connection.LocalIpAddress.MapToIPv4().ToString();
+            var message = $"Hello from Docker Container：{ipv4}";
+
+            return message;
+        }
+
+        [HttpGet("{name}")]
+        public ActionResult<string> Get(string name)
+        {
+            var defaultKey = $"hello:{name}";
+            _distributedCache.SetString(defaultKey, $"Hello {name} form Redis");
+            var message = _distributedCache.GetString(defaultKey);
+
+            return message;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# dockerize-aspnetcore-samples
-The samples show various ways to use .NET Core and Docker together.
+# Dockerize AspNetCore Samples
+## Structure
+![structure](docker-aspnetcore-structure.png)
+
+## Quick Start
+```
+git clone https://github.com/esofar/dockerize-aspnetcore-samples.git
+cd dockerize-aspnetcore-samples
+docker-compose up -d
+```
+
+## Test
+```
+curl http://localhost/api/hello
+curl http://localhost/api/hello
+curl http://localhost/api/hello
+
+// Output:
+
+Hello from Docker Container：172.30.0.2
+Hello from Docker Container：172.30.0.4
+Hello from Docker Container：172.30.0.5
+```
+
+```
+curl http://localhost/api/hello/esofar
+
+// Output:
+
+Hello esofar form Redis
+```
+
